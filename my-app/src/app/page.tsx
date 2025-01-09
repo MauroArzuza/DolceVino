7f5441569aa19f06c712b494a514c1c82d7fa646
JeronimoCortez <jeronimofcortez3@gmail.com>
2025-01-09 12:55:21 -0300

    Seccion productos destacados

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import FeaturedProducts from "./components/FeaturedProducts/FeaturedProducts";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <FeaturedProducts />
+    </>
+  );
 }
