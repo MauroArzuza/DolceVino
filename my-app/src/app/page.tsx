3e216a4372dee05302e842c47e1584047c980ed6
JeronimoCortez <jeronimofcortez3@gmail.com>
2025-01-31 12:23:45 -0300

    Reviews-footer

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import FeaturedProducts from "./components/FeaturedProducts/FeaturedProducts";
+import Reviews from "./components/Reviews/Reviews";
+import Footer from "./components/Footer/Footer";
 
 export default function Home() {
   return (
     <>
-      <FeaturedProducts />
+      {/* <FeaturedProducts /> */}
+      <Reviews />
+      <Footer />
     </>
   );
 }
