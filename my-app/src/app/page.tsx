af3540a790c036c391986de8e5d7da91f8c6bbdd
Ezequiel Argentini <ezeargentini123@gmail.com>
2025-02-03 12:37:34 -0300

    primer update

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
+import Hero from "./components/Hero/Hero";
+import CatalogBox from "./components/CatalogBox";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Hero />
+      <CatalogBox />
+    </div>
+  );
 }
