1b36a611434c97e9412599e254722672bc26cc9b
Ezequiel Argentini <ezeargentini123@gmail.com>
2025-02-18 12:14:24 -0300

    Commit Catalogo, Hero,  Login

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
+import Login from "./components/Login/Login";
 import Hero from "./components/Hero/Hero";
-import CatalogBox from "./components/CatalogBox";
+import CatalogBox from "./components/CatalogBox/CatalogBox";
 
 export default function Home() {
   return (
     <div>
+      <Login />
       <Hero />
       <CatalogBox />
     </div>
