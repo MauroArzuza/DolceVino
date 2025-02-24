33f8e8e17ddad610be68a83b0fe39230125a21bf
JeronimoCortez <jeronimofcortez3@gmail.com>
2025-02-24 11:24:32 -0300

    Carag de imagenes de logos bodegas

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,11 +1,9 @@
 import HomeApp from "./components/screens/HomeApp/HomeApp";
-import ProductByCategory from "./components/screens/ProductByCategory/ProductByCategory";
 
 export default function Home() {
   return (
     <div>
-      {/* <HomeApp /> */}
-      <ProductByCategory />
+      <HomeApp />
     </div>
   );
 }
