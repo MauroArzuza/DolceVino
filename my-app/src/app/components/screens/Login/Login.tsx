c789cf4ded0c24f83f570df2f204e3eb2f7f5da3
Ezequiel Argentini <ezeargentini123@gmail.com>
2025-02-24 12:46:03 -0300

    login y demas screens

--- my-app/src/app/components/screens/Login/Login.tsx
+++ my-app/src/app/components/screens/Login/Login.tsx
@@ -4,25 +4,25 @@ export default function Login() {
   return (
     <div className="w-full h-[600px] bg-[#39342E] flex flex-col items-center justify-center p-6">
       <Image
-        src="/logo-dolce-vino.png"
+        src="/DolceVinoLogo.svg"
         alt="Dolce Vino Logo"
         width={80}
         height={80}
         className="mb-4"
       />
       <h1 className="text-white text-lg font-semibold mb-6">DOLCE VINO</h1>
-      <div className="bg-[#61605B] h-[300px] p-6 rounded-lg w-full max-w-xs">
+      <div className="bg-[#61605B] h-[280px] p-6 rounded-lg w-full max-w-xs">
         <input
           type="text"
           placeholder="Nombre de usuario"
-          className="w-full p-3 mb-4 rounded bg-gray-300 text-black placeholder-gray-600"
+          className="w-full p-3 mb-3 rounded bg-gray-300 text-black placeholder-gray-600"
         />
         <input
           type="password"
           placeholder="Contraseña"
-          className="w-full p-3 mb-4 rounded bg-gray-300 text-black placeholder-gray-600"
+          className="w-full p-3 mb-3 rounded bg-gray-300 text-black placeholder-gray-600"
         />
-        <button className="w-32 bg-yellow-500 text-black font-semibold py-2 rounded hover:bg-yellow-600 mx-auto block">
+        <button className="w-28 bg-[#BBC620] text-black font-semibold py-2 rounded hover:bg-yellow-400 mx-auto block mt-4">
           Ingresar
         </button>
       </div>
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,13 @@
 import Login from "./components/Login/Login";
 import HomeApp from "./components/screens/HomeApp/HomeApp";
+import EditProducts from "./components/EditProducts/EditProducts";
+import CreateProducts from "./components/CreateProducts/CreateProducts";
 
 export default function Home() {
   return (
     <div>
+      <CreateProducts />
+      <EditProducts />
       <Login />
       <HomeApp />
     </div>
